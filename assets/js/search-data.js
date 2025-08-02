52d0f7f119a1fa07578777f3c22f24fcef19005b
sbmldev <mjlns.suresh@gmail.com>
2025-08-02 08:20:41 +0000

    Deploying to gh-pages from @ sbmldev/sbml-site@c4648e424f81887a489093cea5c4d2e8c5418ac7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-my-first-post",
+        
+          title: "my first post",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/my-first-post/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
